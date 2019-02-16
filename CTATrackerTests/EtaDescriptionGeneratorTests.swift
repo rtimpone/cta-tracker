92bd2ab29efe9adb56b6412e5716355759b0bd7b
Rob Timpone <rtimpone@gmail.com>
2019-02-16 12:11:17 -0600

    Minor UI tweaks, adding unit tests placeholder for eta cell

---
+++ CTATrackerTests/EtaDescriptionGeneratorTests.swift
@@ -0,0 +1,20 @@
+//
+//  EtaDescriptionGeneratorTests.swift
+//  CTATrackerTests
+//
+//  Created by Rob Timpone on 2/16/19.
+//  Copyright © 2019 Rob Timpone. All rights reserved.
+//
+
+import XCTest
+import CTAKit
+@testable import CTA
+
+class EtaDescriptionGeneratorTests: XCTestCase {
+    
+    func testArrivalInThirtySeconds() {
+        
+//        let eta = ETA(
+//        let
+    }
+}
