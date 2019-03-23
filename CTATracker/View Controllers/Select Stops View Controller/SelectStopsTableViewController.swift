e959800f92c1f11f5804171396b3b4efadd17508
Rob Timpone <rtimpone@gmail.com>
2019-03-23 12:12:16 -0500

    Displaying platforms alongside stations in table view

--- CTATracker/View Controllers/Select Stops View Controller/SelectStopsTableViewController.swift
+++ CTATracker/View Controllers/Select Stops View Controller/SelectStopsTableViewController.swift
@@ -17,35 +17,57 @@ protocol SelectStopsTableViewControllerDelegate: class {
 class SelectStopsTableViewController: UITableViewController {
     
     weak var delegate: SelectStopsTableViewControllerDelegate?
-    var stations: [Station] = []
+    var stops: [Stop] = []
     
     func displayStations(_ stations: [Station]) {
-        self.stations = stations
-        refreshStations()
+        var stops: [Stop] = []
+        for station in stations {
+            stops.append(station)
+            stops.append(contentsOf: station.platforms)
+        }
+        self.stops = stops
+        refreshStops()
     }
     
-    func refreshStations() {
+    func refreshStops() {
         tableView.reloadData()
     }
     
     // MARK: Table View Data Source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return stations.count
+        return stops.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let station = stations[indexPath.row]
-        let cell = tableView.dequeueReusableCell(ofType: StationCell.self)
-        let isSelected = delegate?.stopIsSelected(station) ?? false
-        cell.configure(for: station, isSelected: isSelected)
-        return cell
+        let stop = stops[indexPath.row]
+        let isSelected = delegate?.stopIsSelected(stop) ?? false
+        return configuredCell(forStop: stop, isSelected: isSelected)
     }
     
     // MARK: Table View Delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let station = stations[indexPath.row]
-        delegate?.didSelectStop(station)
+        let stop = stops[indexPath.row]
+        delegate?.didSelectStop(stop)
+    }
+}
+
+private extension SelectStopsTableViewController {
+    
+    func configuredCell(forStop stop: Stop, isSelected: Bool) -> UITableViewCell {
+        if let station = stop as? Station {
+            let cell = tableView.dequeueReusableCell(ofType: StationCell.self)
+            cell.configure(for: station, isSelected: isSelected)
+            return cell
+        }
+        else if let platform = stop as? Platform {
+            let cell = tableView.dequeueReusableCell(ofType: PlatformCell.self)
+            cell.configure(for: platform, isSelected: isSelected)
+            return cell
+        }
+        else {
+            fatalError("Invalid stop found in data source: \(stop)")
+        }
     }
 }
